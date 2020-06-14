31101c9e4b007500c8fc5c6c1c9439f75fd17fe5
mona95 <shamona9594@gmail.com>
2020-06-14 17:24:32 +0300

    provided guide comments and re ordering scripts tags in index.html

--- modules/eventsModule.js
+++ modules/eventsModule.js
@@ -8,6 +8,19 @@ let eventsModule = (function (dModule, uModule, cModule, wModule) {
   return {
     //init function , initialize the test before start
     init: function (duration, textNumber) {
+      // STEPS :
+
+      //fill the list of test words : in data module
+      //fill the list of test words : in ui module
+      // set the total time test
+      // update the time left : in data module
+      // update the time left : in ui module
+      //move to a new word : in data module
+      // set active word : in ui module
+      //format the active module : in ui module
+      //focus on text input : in ui module
+      //add event listeners
+
       addEventListeners();
     },
   };

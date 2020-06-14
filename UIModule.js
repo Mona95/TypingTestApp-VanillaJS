47fef936ca5b62f8c3a286fc79550ff1faab14db
mona95 <shamona9594@gmail.com>
2020-06-14 13:37:18 +0300

    UI Module structure

---
+++ UIModule.js
@@ -0,0 +1,51 @@
+let UIModule = (function () {
+  //classes used to select HTML Elements
+  let DOMElements = {
+    //indicators - test control
+    timeLeft, //HTML element displaying time left
+    //test results
+    wpm,
+    wpmChange,
+    cpm,
+    cpmChange,
+    accurancy,
+    accurancyChange,
+    //user input
+    textInput,
+    nameInput,
+    //test words
+    content,
+    activeWord,
+    //modal
+    modal,
+  };
+
+  return {
+    //get DOM elements
+    getDOMElements: function () {},
+
+    //indicators - Test Control
+    updateTimeLeft: function () {},
+
+    //results
+    updateResults: function () {},
+    fillModal: function () {},
+    showModal: function () {},
+
+    //user input
+    inputFocus: function () {},
+    isNameEmpty: function () {},
+    flagNameInput: function () {},
+    spacePressed: function () {},
+    enterPressed: function () {},
+    emptyInput: function () {},
+    getTypedWord: function () {},
+
+    //test words
+    fillContent: function () {},
+    formatWord: function (wordObject, wordHTML) {},
+    setActiveWord: function (index) {},
+    deactivateCurrentWord: function () {},
+    scroll: function () {},
+  };
+})();

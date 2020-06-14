7e9bc90ee403d067fb5b8354d7d8ee03d5f93051
mona95 <shamona9594@gmail.com>
2020-06-14 13:39:35 +0300

    events module strtucture

---
+++ eventsModule.js
@@ -0,0 +1,14 @@
+let eventsModule = (function () {
+  let addEventListeners = function () {
+    //character typing event listener
+    //click on download button event listener
+    // click on restart button event listener
+  };
+
+  return {
+    //init function , initialize the test before start
+    init: function (duration, textNumber) {
+      addEventListeners();
+    },
+  };
+})();
